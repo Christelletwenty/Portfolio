f187c7a10669f5619e04cc8f4eebe30b9c62b6a1
ChrisGigaB <christellegigabento@gmail.com>
2024-07-12 16:50:43 +0200

    feat(FormularComponent): working with emailjs

--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -1,25 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-formular',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './formular.component.html',
-  styleUrl: './formular.component.scss'
+  styleUrl: './formular.component.scss',
 })
-export class FormularComponent {
-  profileForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    message: new FormControl('', Validators.required),
+export class FormularComponent implements OnInit {
+  private fb = inject(FormBuilder);
+
+  public emailIsSending = false;
+
+  public form = this.fb.group({
+    from_name: ['', Validators.required],
+    from_email: ['', [Validators.required, Validators.email]],
+    message: ['', Validators.required],
   });
 
-  handleSubmit() {
-    alert(
-      this.profileForm.value.name + ' | ' + this.profileForm.value.message +' | ' + this.profileForm.value.email
-    );
+  public ngOnInit(): void {
+    emailjs.init('AqRK-NRfLxsXeFGj5');
+  }
+
+  public send(): void {
+    if (this.form.valid || this.form.pristine) {
+      this.emailIsSending = true;
+
+      // Observable way
+      // from(emailjs.send('service_omg40z5', 'template_r3hdhnc', {
+      //   from_name: this.form.value.from_name,
+      //   to_name: '',
+      //   from_email: this.form.value.from_email,
+      //   message: this.form.value.message,
+      // })).subscribe({
+      //   next: () => {
+      //     this.emailIsSending = false;
+      //     alert('Le message a bien été envoyé.');
+      //     this.form.reset();
+      //   },
+      //   error: (err) => {
+      //     alert('Une erreur est survenue, veuillez réessayer plus tard');
+      //     console.error(err);
+      //   }
+      // });
+
+      // Promise way
+      emailjs
+        .send('service_omg40z5', 'template_r3hdhnc', {
+          from_name: this.form.value.from_name,
+          to_name: '',
+          from_email: this.form.value.from_email,
+          message: this.form.value.message,
+        })
+        .then(
+          () => {
+            this.emailIsSending = false;
+            alert('Le message a bien été envoyé.');
+            this.form.reset();
+          },
+          (err) => {
+            alert('Une erreur est survenue, veuillez réessayer plus tard');
+            console.error(err);
+          }
+        );
+    }
   }
 }
